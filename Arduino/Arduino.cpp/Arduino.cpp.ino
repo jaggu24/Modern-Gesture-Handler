6a1d848b6837b40d361a8ec2cdd75d8c132c5a74
manoj2210 <45814826+manoj2210@users.noreply.github.com>
2019-10-21 16:36:45 +0530

    Fixed the bugs and completed the internal parts

--- Arduino/Arduino.cpp/Arduino.cpp.ino
+++ Arduino/Arduino.cpp/Arduino.cpp.ino
@@ -2,12 +2,10 @@
 
 #define trigPin1 2
 #define echoPin1 3
-
 #define trigPin2 7
 #define echoPin2 8
 
 ////Combinations
-
 //Left and Right stop for a short time -2
 //Left and Right forward motion        -2
 //Left and Right backward motion       -2
@@ -19,45 +17,24 @@
 //Both hands forward and backward      -2
 
 //                                      =14
-
-
-int mean1=0,mean2=0;
-String m1,m2;
-bool initial=false,flag=false;
-int i=0,o=0;
-long duration1;
+int median1=0,median2=0;  //median of data
+bool initial=false;  //initial start condition
+int i=0; //array assignment
+long duration1;   
 long duration2;
 int distance1;
 int distance2;
 int start=-1;
 int Data_array1[100],Data_array2[100];
 
-void setup(){
 
-pinMode(trigPin1, OUTPUT); // Sets the trigPin1 as an Output
-pinMode(trigPin2, OUTPUT); // Sets the trigPin2 as an Output
-pinMode(13, OUTPUT); 
-pinMode(echoPin1, INPUT); // Sets the echoPin1 as an Input
-pinMode(echoPin2, INPUT); // Sets the echoPin2 as an Input
-Serial.begin(115200); // Starts the serial communication
-
-}
 
 void startfun(){
   if (Serial.available() > 0 ) {    //Start the Data input 
     start = Serial.read()-48;
     digitalWrite(13,HIGH);
+    initial=true;
   }
-}
-void meancal(){
-  if(mean1&&mean2){
-       mean1=(m1[0]-48)*10+(m1[1]-48);
-       mean2=(m2[0]-48)*10+(m2[1]-48);
-//       Serial.println("knkjnj"+m1);
-  //     delay(1000);
-//       Serial.println("knkddsds"+m2);
-       
-    }
 }
 
 void distancecal(){
@@ -84,87 +61,81 @@ void distancecal(){
     distance1= duration1*0.034/2;
     distance2= duration2*0.034/2;
 }
-
-void normaldata(){
-  
-      if(distance1<mean1){
-        Data_array1[i]=distance1;    
-        if(distance2<mean2){
-          Data_array2[i]=distance2;
-        }
-        else{
-          Data_array2[i]=0;
-        }
-      }
-      else{
-        Data_array2[i]=distance2;
-        Data_array1[i]=0;
-      }   
-    
-}
+//
+//void normaldata(){
+//  
+//      if(distance1<median1){
+//        Data_array1[i]=distance1;    
+//        if(distance2<mean2){
+//          Data_array2[i]=distance2;
+//        }
+//        else{
+//          Data_array2[i]=0;
+//        }
+//      }
+//      else{
+//        Data_array2[i]=distance2;
+//        Data_array1[i]=0;
+//      }   
+//    
+//}
 
 void initialdata(){
   
        digitalWrite(13,HIGH);
-       for(int m=0;m<i;m++){
-         delay(10);
-         Serial.println(Data_array1[m]);
-         delay(10);
-         Serial.println(Data_array2[m]);
-       }
-      i=0;
-      o++;
-      if(o==3){
-        initial=true;
-      
-    }
+       //Median finding algo
+       median1=20;
+       median2=20;
 }
 
-void meanreceive(){
-  if (Serial.available() > 0 && !mean1 && !mean2) {    //Start the Data input 
-        digitalWrite(13,HIGH);
-        m1 = Serial.read();
-        mean1=1;
-        delay(1);
-        m2 = Serial.read(); 
-         mean2=1;
-         if(mean1)
-         initial=false;
-      }
-          
+void setup(){
+
+pinMode(trigPin1, OUTPUT); // Sets the trigPin1 as an Output
+pinMode(trigPin2, OUTPUT); // Sets the trigPin2 as an Output
+pinMode(13, OUTPUT); 
+pinMode(echoPin1, INPUT); // Sets the echoPin1 as an Input
+pinMode(echoPin2, INPUT); // Sets the echoPin2 as an Input
+Serial.begin(1152000); // Starts the serial communication
+
 }
+
 void loop(){
   
-  if(!initial&&!mean1&&!mean2){
+  if(!initial){
     startfun();
   }
-  
-  if(start==1){
+  if(start!=-1){
+    Serial.setTimeout(50);
     distancecal();
-    if(mean1&&mean2&&!flag){
-      digitalWrite(13,LOW);
-      Serial.println(m1+m2+"hfkjdhfjdsgj");
-//      meancal();
-//      flag=true;
-//     Serial.println(m1+"ljkfjklj"+m2+"kjgfjhvjh");
-    }
-    
-    if(!mean1&&!mean2){
-
+  
+    if(!median1&&!median2){
       Data_array1[i]=distance1;
       Data_array2[i]=distance2;
       i++;
+      
+      if(i>100){
+        initialdata();  
+        i=0; 
+      }
     }
     
-    
-
-    if(i>100&&!mean1&&!mean2){
-     initialdata();   
-    }
-    
-    if(initial){
-       meanreceive();
+    else{
+      digitalWrite(13,LOW);
+      if(distance1<median1){
+        //Transmit data
+        Serial.println(String(distance1)+'1'+String(i));
+        delay(10);  
+        i++;
+      }
+      if(distance2<median2){
+        //Transmit data
+        Serial.println(String(distance2)+'2'+String(i));
+        delay(10);
+        i++;
+      }
     }
   }
 }
+
+
   
--- Gesture.py
+++ Gesture.py
@@ -2,75 +2,81 @@ import serial #Serial imported for Serial communication
 import time #Required to use delay functions
 import statistics as stat
 
-ArduinoSerial = serial.Serial('com3',115200,timeout=5)
+ArduinoSerial = serial.Serial('com3',baudrate=1152000,timeout=50)
 
 time.sleep(2)
 ArduinoSerial.write('1'.encode('utf-8'))
 
-initial_data1=[]
-initial_data2=[]
-
-initial_bool1=False
-initial_bool2=False
-
-median1=0
-median2=0
-
-m=0
-
-# ArduinoSerial.timeout(float(1000))
 while 1:
-     if(initial_bool1==0 and initial_bool2==0):
-          if(ArduinoSerial.readline()):
-               if(m%2):
-                    initial_data2.append(ArduinoSerial.readline())
-                    print((ArduinoSerial.readline()))
-               else:
-                    initial_data1.append(ArduinoSerial.readline())
-                    print((ArduinoSerial.readline()))
-               m+=1
+     if(ArduinoSerial.readline()):
+          # print(ArduinoSerial.readline())
+          m=str(ArduinoSerial.readline())
+          m=m[2:3]
           print(m)
+# initial_data1=[]
+# initial_data2=[]
 
-          if(len(initial_data1)==100 and initial_bool2==False):
-               initial_data=[str(x) for x in initial_data1 ]
-               initial_data=[x.replace("b\'","") for x in initial_data]
-               initial_data=[x.replace("\\r\\n\'","") for x in initial_data]
-               for x in range(len(initial_data)):
-                    try:
-                         initial_data[x]=int(initial_data[x])
-                    except:
-                         initial_data[x]=0
-               median1=stat.median(initial_data)
-               print("Mean1",median1)
-               initial_bool2=True
-          # exit()
+# initial_bool1=False
+# initial_bool2=False
+
+# median1=0
+# median2=0
+
+# m=0
+
+# # ArduinoSerial.timeout(float(1000))
+# while 1:
+#      if(initial_bool1==0 and initial_bool2==0):
+#           if(ArduinoSerial.readline()):
+#                if(m%2):
+#                     initial_data2.append(ArduinoSerial.readline())
+#                     print((ArduinoSerial.readline()))
+#                else:
+#                     initial_data1.append(ArduinoSerial.readline())
+#                     print((ArduinoSerial.readline()))
+#                m+=1
+#           print(m)
+
+#           if(len(initial_data1)==100 and initial_bool2==False):
+#                initial_data=[str(x) for x in initial_data1 ]
+#                initial_data=[x.replace("b\'","") for x in initial_data]
+#                initial_data=[x.replace("\\r\\n\'","") for x in initial_data]
+#                for x in range(len(initial_data)):
+#                     try:
+#                          initial_data[x]=int(initial_data[x])
+#                     except:
+#                          initial_data[x]=0
+#                median1=stat.median(initial_data)
+#                print("Mean1",median1)
+#                initial_bool2=True
+#           # exit()
      
-          if(len(initial_data1)==100 and initial_bool1==False):
-               initial_data=[str(x) for x in initial_data2 ]
-               initial_data=[x.replace("b\'","") for x in initial_data]
-               initial_data=[x.replace("\\r\\n\'","") for x in initial_data]
+#           if(len(initial_data1)==100 and initial_bool1==False):
+#                initial_data=[str(x) for x in initial_data2 ]
+#                initial_data=[x.replace("b\'","") for x in initial_data]
+#                initial_data=[x.replace("\\r\\n\'","") for x in initial_data]
           
-               for x in range(len(initial_data)):
-                    try:
-                         initial_data[x]=int(initial_data[x])
-                    except:
-                         initial_data[x]=0
-               median2=stat.median(initial_data)
-               print("Mean2",median2)
-               initial_bool1=True
-          # exit()
+#                for x in range(len(initial_data)):
+#                     try:
+#                          initial_data[x]=int(initial_data[x])
+#                     except:
+#                          initial_data[x]=0
+#                median2=stat.median(initial_data)
+#                print("Mean2",median2)
+#                initial_bool1=True
+#           # exit()
      
-     if(initial_bool1 and initial_bool2):
-          ArduinoSerial.write(str(median1).encode('utf-8'))
-          # sleep(1)
-          ArduinoSerial.write(str(median2).encode('utf-8'))
-          ArduinoSerial.flush()
-          if(ArduinoSerial.readline()):
-               print("m1",(ArduinoSerial.readline()))
+#      if(initial_bool1 and initial_bool2):
+#           ArduinoSerial.write(str(median1).encode('utf-8'))
+#           # sleep(1)
+#           ArduinoSerial.write(str(median2).encode('utf-8'))
+#           ArduinoSerial.flush()
+#           if(ArduinoSerial.readline()):
+#                print("m1",(ArduinoSerial.readline()))
           
-          # if(ArduinoSerial.readline()):
-          #      print("m2",(ArduinoSerial.readline()))
-          #      # exit()
+#           # if(ArduinoSerial.readline()):
+#           #      print("m2",(ArduinoSerial.readline()))
+#           #      # exit()
           
 
      
